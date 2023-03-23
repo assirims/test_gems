8048a788842b993b8f4f2605f13a216c296d3ee0
Mohammed Assiri <q2me@live.com>
2023-03-23 20:30:57 +0000

    update host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: '10.0.0.8', port: 8080 }
+  config.action_mailer.default_url_options = { host: 'http://10.0.0.8', port: 8080 }
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
