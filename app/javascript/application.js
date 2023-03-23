4934fc752facde357d1f70119974b86414cef3c1
Mohammed Assiri <q2me@live.com>
2023-03-23 02:16:00 +0000

    rich text with bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "trix"
+import "@rails/actiontext"
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
-import "@fortawesome/fontawesome-free/css/all"
+// import * as bootstrap from "bootstrap"
+// import "@fortawesome/fontawesome-free/css/all"
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   validates :title, :short_description, :language, :price, :level, presence: true
   validates :description, presence: true, length: { minimum: 5 }
+  has_rich_text :description
 
   # friendly_id
   extend FriendlyId
