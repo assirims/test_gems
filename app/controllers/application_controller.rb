68bb4faab60e999935c50844b80ddcb0ce9f3027
Mohammed Assiri <q2me@live.com>
2023-03-24 02:41:29 +0000

    navbar search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :set_global_search_variable, if: :user_signed_in?
+  def set_global_search_variable
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+    @ransack_users = User.ransack(params[:users_search], search_key: :users_search) #navbar search
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
     # @users = User.all.order(created_at: :desc)
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    # @q = User.ransack(params[:q])
+    # @users = @q.result(distinct: true)
+    @ransack_users = User.ransack(params[:users_search], search_key: :users_search)
+    @users = @ransack_users.result.includes(:courses)
   end
 end
