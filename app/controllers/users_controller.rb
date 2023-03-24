24e5327934cac977e917b865bc53640359385719
Mohammed Assiri <q2me@live.com>
2023-03-24 23:56:05 +0000

    edit role with switch box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
   def index
     # @users = User.all.order(created_at: :desc)
     # @q = User.ransack(params[:q])
@@ -7,4 +8,30 @@ class UsersController < ApplicationController
     @ransack_users.sorts = ['id desc'] if @ransack_users.sorts.empty?
     @users = @ransack_users.result.includes(:courses)
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully destroyed.'
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({role_ids: []})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index , :edit, :show, :update, :destroy]
 
   get 'home/activity'
   match '/privacy', to: 'home#privacy_policy', via: 'get'
