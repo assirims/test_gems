5e4e6787267e558db64af3328631a4fbb5c97fb5
Mohammed Assiri <q2me@live.com>
2023-03-25 01:06:04 +0000

    test role-based access

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,8 +11,12 @@ class CoursesController < ApplicationController
       # @q = Course.ransack(params[:q])
       # @courses = @q.result(distinct: true)
     # end
-    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
-    @courses = @ransack_courses.result.includes(:user)
+    # if current_user.has_role? :admin
+      @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+      @courses = @ransack_courses.result.includes(:user)
+    # else
+    #   redirect_to root_path, alert: "You are not authorized to access this page."
+    # end
   end
 
   # GET /courses/1
