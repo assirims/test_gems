14501887d6a654918d0781a4618393e4db4ddfc1
Mohammed Assiri <q2me@live.com>
2023-03-26 09:42:41 +0000

    styling and improving code

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,9 +4,6 @@ class LessonsController < ApplicationController
 
   # GET /lessons
   def index
-    @ransack_lessons = Lesson.ransack(params[:lessons_search], search_key: :lessons_search)
-    # @ransack_lessons.sorts = ['id desc'] if @ransack_lessons.sorts.empty?
-    @lessons = @ransack_lessons.result(distinct: true)
   end
 
   # GET /lessons/1
@@ -49,7 +46,7 @@ class LessonsController < ApplicationController
   # DELETE /lessons/1
   def destroy
     @lesson.destroy
-    redirect_to course_lessons_url, notice: "Lesson was successfully destroyed."
+    redirect_to course_path(@course), notice: "Lesson was successfully destroyed."
   end
 
   private
@@ -61,7 +58,7 @@ class LessonsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def lesson_params
-      params.require(:lesson).permit(:title, :content, :course_id)
+      params.require(:lesson).permit(:title, :content)
     end
 
     def authorize_valuations
