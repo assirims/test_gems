382ed4ee2f3452a990d872ab64c6e9c155152d81
Mohammed Assiri <q2me@live.com>
2023-04-12 23:21:40 +0000

    gem public_activity for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,14 @@ class Comment < ApplicationRecord
 
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
-  # User.find_each { |user| User.reset_counters(user.id, :comments) }
-  # Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
 
   validates :content, presence: true
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    content
+  end
 
 end
